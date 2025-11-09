000df32f3c4c9d7fab020bf7ec933e9de04fc3cf
omchoksi108 <omchoksi108@gmail.com>
2025-11-09 11:47:32 +0530

    Add public setting to vercel.json and update CORS

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,7 @@ const corsOptions = {
     'https://workzen-lmn1xwpdc-om-choksi-s-projects.vercel.app',
     'https://workzen-5q7l7zc13-om-choksi-s-projects.vercel.app',
     'https://workzen-x9wniy7pa-om-choksi-s-projects.vercel.app',
+    'https://workzen-9qej6f849-om-choksi-s-projects.vercel.app',
     'http://localhost:5173',
     'http://localhost:3000'
   ],
